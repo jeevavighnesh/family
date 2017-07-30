6505215793dd44f5f50cd0ca055fa01599de4f91
jeevavighnesh <jeevavignesh@gmail.com>
2017-07-30 11:10:15 +0530

    Bug Fixes

--- api/controllers/gitChangesController.js
+++ api/controllers/gitChangesController.js
@@ -5,12 +5,12 @@ var exec = require('child_process').exec;
 module.exports = {
     onPushedChanges: function (req, resp) {
         console.log(req.body.pusher.name + " pushed to " + req.body.repository.name);
-        console.log("Pulling from GitHub " + req.body.repository.name + " repository");
         exec('echo "$(date) INFO: ' + req.body.pusher.name + ' pushed to ' + req.body.repository.name + '" >> ../family_logs/info.log', exeCallback);
-        exec('git -C ~/Documents/family reset --hard', exeCallback);
-        exec('git -C ~/Documents/family clean -df', exeCallback);
+        console.log("Pulling from GitHub " + req.body.repository.name + " repository");
+        // exec('git -C ~/Documents/family reset --hard', exeCallback);
+        // exec('git -C ~/Documents/family clean -df', exeCallback);
         exec('git -C ~/Documents/family pull -f', exeCallback);
-        exec('npm -C ~/Documents/family install --production', exeCallback);
+        // exec('npm -C ~/Documents/family install --production', exeCallback);
         exec('echo "$(date): $SECONDS" >> ../family_logs/info.log', exeCallback)
         exec('sudo ./restart_server.sh', exeCallback);
         resp.sendStatus(200);
