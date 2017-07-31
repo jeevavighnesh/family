560a9cba0545fc98f1b124d5d25d823e0090bd6a
jeevavighnesh <jeevavignesh@gmail.com>
2017-07-31 08:24:22 +0530

    Testing...

--- api/routes/TestRoute.js
+++ api/routes/TestRoute.js
@@ -3,6 +3,6 @@ module.exports = function (app) {
     var relationController = require("../controllers/relationController");
 
     app.route('/test').get(function (req, resp) {
-        resp.send("Hai");
+        resp.send("Hello Mama");
     });
 };
